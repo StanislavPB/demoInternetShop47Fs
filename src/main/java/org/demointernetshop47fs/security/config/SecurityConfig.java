2f31fef90fc85283a217ec772143608df3eb02db
StanislavPB <perminovsb@gmail.com>
2024-11-07 14:56:12 +0000

    add controller tests

--- src/main/java/org/demointernetshop47fs/security/config/SecurityConfig.java
+++ src/main/java/org/demointernetshop47fs/security/config/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .exceptionHandling(eh -> eh
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.FORBIDDEN)))
+                .addFilterBefore(new BasicAuthFilter(authenticationManager(http.getSharedObject(AuthenticationConfiguration.class))), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
